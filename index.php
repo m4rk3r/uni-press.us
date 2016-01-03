c10878e0f7c6f4ac1241c71ef60056a829e4614f
m4rk3r <mbeasl1@gmail.com>
2016-01-03 11:39:25 -0600

    Added index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+<style type='text/css'>
+  body {
+     font-size:24px;
+     font-family:times;
+  }
+  #msg-bin{
+      display:flex;
+
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+
+      height:98%;
+  }
+  #hi {
+      font-size:200px;
+  }
+</style>
+
+  <div id="font-detect-swf"></div>
+
+  <script src="js/jquery-1.11.2.min.js"></script>
+  <script src="js/swfobject.js"></script>
+  <script src="js/font-detect.js"></script>
+  <script src="js/underscore-min.js"></script>
+  <script>
+    var fonts = [];
+
+    $(document).ready(function() {
+        if(typeof localStorage.unifonts === "undefined"){
+            if (!swfobject.hasFlashPlayerVersion("9.0.0"))
+                return;
+
+            var fontDetect = new FontDetect("font-detect-swf", "flash/FontList.swf", function(fd) {
+                fonts = _.pluck(fd.fonts(),'fontName');
+                // store so that we dont always have to load em ;)
+                localStorage.unifonts = fonts;
+            });
+        }else{
+            fonts = localStorage.unifonts.split(',');
+        }
+
+        var hi = $('#hi');
+        var i = 0;
+
+        setInterval(function (){
+            hi.css('font-family',fonts[i++]);
+            if(i > fonts.length) i = 0;
+        },100);
+
+    });
+</script>
+</head>
+<body>
+<div style='position:absolute;left:10px;top:10px;'>
+uni-press.us
+</div>
+<div style=position:absolute;right:10px;top:10px;'>
+studio for research and production
+</div>
+<div style='position:absolute;left:10px;bottom:10px;'>
+  jasmine.e.lee [ on ] gmail.com
+</div>
+<div style='position:absolute;right:10px;bottom:10px;'>
+  m4rk.beasley [ around ] gmail.com
+</div>
+
+<div id='msg-bin'>
+    <div id='hi'>hi!</div>
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1247021-7', 'uni-press.us');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
